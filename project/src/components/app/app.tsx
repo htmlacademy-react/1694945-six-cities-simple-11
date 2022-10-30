8e4beb76a0b7d5038d4127ae63e2035c56edbd51
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-10-30 14:36:15 +0700

    Добавлены маршруты на остальные страницы в главный компонент

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,10 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import PropertyPage from '../../pages/property-page/property-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
+
 
 type AppPageProps = {
   cardsCount: number;
@@ -15,6 +18,8 @@ function App({ cardsCount }: AppPageProps): JSX.Element {
           path={AppRoute.Main}
           element={<MainPage cardsCount={cardsCount} />}
         />
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path={AppRoute.Room} element={<PropertyPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
