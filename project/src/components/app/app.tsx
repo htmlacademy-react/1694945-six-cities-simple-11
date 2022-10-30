905f694ca3994aae1bdda1b3f60476c55d1b896f
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-10-30 13:47:29 +0700

    Созданы 2 маршрута — главная страница и ошибка 404

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,23 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 type AppPageProps = {
   cardsCount: number;
 };
 
 function App({ cardsCount }: AppPageProps): JSX.Element {
-  return <MainPage cardsCount={cardsCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage cardsCount={cardsCount} />}
+        />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
