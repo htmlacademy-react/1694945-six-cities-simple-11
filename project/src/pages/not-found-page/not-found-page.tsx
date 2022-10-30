bcce69b3ae0195f0eea01218bda0564ba194d641
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-10-30 14:32:45 +0700

    Компонент для логотипа сайта внесён в разметку страниц

--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -1,12 +1,26 @@
 import { Link } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <div
-      style={{ margin: '100px auto', maxWidth: '500px', textAlign: 'center' }}
-    >
-      <h1>Error 404 — Page Not Found</h1>
-      <Link to='/' style={{ textDecoration: 'underline' }}>Return to Main Page</Link>
+    <div className="page page--gray">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Logo />
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="page__main page__main">
+        <div
+          style={{ margin: '100px auto', maxWidth: '500px', textAlign: 'center' }}
+        >
+          <h1>Error 404 — Page Not Found</h1>
+          <Link to='/' style={{ textDecoration: 'underline' }}>Return to Main Page</Link>
+        </div>
+      </main>
     </div>
   );
 }
