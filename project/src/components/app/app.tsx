663d08e790d9cde7f96073b25b09350bb43ce911
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-10-31 20:09:23 +0700

    Импортированы приватные маршруты и число карточек на странице

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,7 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { HelmetProvider } from 'react-helmet-async';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 import MainPage from '../../pages/main-page/main-page';
 import LoginPage from '../../pages/login-page/login-page';
 import PropertyPage from '../../pages/property-page/property-page';
@@ -8,21 +10,42 @@ import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 type AppPageProps = {
   cardsCount: number;
+  cardsOnPage: number;
 };
 
-function App({ cardsCount }: AppPageProps): JSX.Element {
+const IS_AUTHORIZED = true;
+
+function App({ cardsCount, cardsOnPage }: AppPageProps): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path={AppRoute.Main}
-          element={<MainPage cardsCount={cardsCount} />}
-        />
-        <Route path={AppRoute.Login} element={<LoginPage />} />
-        <Route path={AppRoute.Room} element={<PropertyPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Main}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.Auth}
+              >
+                <MainPage isAuthorized={IS_AUTHORIZED} cardsCount={cardsCount} cardsOnPage={cardsOnPage} />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoute.Login} element={<LoginPage />} />
+
+          <Route
+            path={AppRoute.Room}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.Auth}
+              >
+                <PropertyPage isAuthorized={IS_AUTHORIZED} />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoute.NotFound} element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 export default App;
