9941fbb67cb8263786c3fc5c02318965d478f6d3
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-10-31 20:12:17 +0700

    Создан компонент навигационной панели

---
+++ project/src/components/nav/nav.tsx
@@ -0,0 +1,42 @@
+import { AppRoute } from '../../const';
+
+type NavComponentProps = {
+  isAuthorized: boolean;
+};
+
+function Nav({ isAuthorized }: NavComponentProps): JSX.Element {
+  return (
+    <nav className="header__nav">
+      <ul className="header__nav-list">
+        {isAuthorized ? (
+          <>
+            <li className="header__nav-item user">
+              <div className="header__nav-profile">
+                <div className="header__avatar-wrapper user__avatar-wrapper" />
+                <span className="header__user-name user__name">
+                  Oliver.conner@gmail.com
+                </span>
+              </div>
+            </li>
+            <li className="header__nav-item">
+              <a className="header__nav-link" href={AppRoute.Login}>
+                <span className="header__signout">Sign out</span>
+              </a>
+            </li>
+          </>
+        ) : (
+          <li className="header__nav-item user">
+            <a
+              className="header__nav-link header__nav-link--profile"
+              href={AppRoute.Login}
+            >
+              <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+              <span className="header__login">Sign in</span>
+            </a>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
+export default Nav;
