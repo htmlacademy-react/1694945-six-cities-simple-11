0ccf6c95dc3cee7cca89b7bc3e58aacddc616f5a
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-09 12:56:52 +0700

    Подключены вымышленные данные

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
-enum Setting {
-  CardsCount = 312,
-  CardsOnPage = 6
-}
+import { offers } from './mocks/offers';
+import { reviews } from './mocks/reviews';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App cardsCount={Setting.CardsCount} cardsOnPage={Setting.CardsOnPage} />
+    <App
+      offers={offers}
+      reviews={reviews}
+    />
   </React.StrictMode>,
 );
