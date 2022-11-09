748f3aaaee7d8400250ab0d3320680018c9ee1e1
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-09 13:35:20 +0700

    Подключены компоненты предложений и константы

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,74 +1,59 @@
 import { Helmet } from 'react-helmet-async';
+import HeaderSvg from '../../components/header/header-svg';
 import Header from '../../components/header/header';
-import Cities from '../../components/cities/cities';
-import CitiesEmpty from '../../components/cities/cities-empty';
+import OffersList from '../../components/offer/offers-list';
+import NoOffers from '../../components/offer/no-offers';
+import { Offers } from '../../types/offer';
+import { CITIES, ACTIVE_CITY } from '../../const';
+
 
 type MainPageProps = {
-  cardsCount: number;
-  cardsOnPage: number;
+  offers: Offers;
   isAuthorized: boolean;
 };
 
-function MainPage({ cardsCount, cardsOnPage, isAuthorized }: MainPageProps): JSX.Element {
+function MainPage({ isAuthorized, offers }: MainPageProps): JSX.Element {
+  const filteredOffers = offers.filter((offer) => offer.city.name === ACTIVE_CITY);
+  const mainClassName =
+    filteredOffers.length > 0
+      ? 'page page--gray page--main'
+      : 'page__main page__main--index page__main--index-empty';
+  const citiesList = CITIES.map((city, index) => ({
+    id: index + 1,
+    name: city,
+  })).map((city) => (
+    <li key={city.id} className="locations__item">
+      <a
+        className={`locations__item-link tabs__item
+          ${city.name === ACTIVE_CITY ? ' tabs__item--active' : ''}`}
+        href="#todo"
+      >
+        <span>{city.name}</span>
+      </a>
+    </li>
+  ));
+  const offersList =
+    offers.length > 0
+      ? <OffersList offers={offers} city={ACTIVE_CITY} />
+      : <NoOffers city={ACTIVE_CITY} />;
   return (
     <div className="page page--gray page--main">
       <Helmet>
         <title>Main Page</title>
       </Helmet>
+      {offers.length > 0 && <HeaderSvg />}
       <Header isAuthorized={isAuthorized} />
-      <main
-        className={
-          cardsCount > 0
-            ? 'page page--gray page--main'
-            : 'page__main page__main--index page__main--index-empty'
-        }
-      >
+      <main className={mainClassName}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
             <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="/">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="/">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="/">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a
-                  className="locations__item-link tabs__item tabs__item--active"
-                  href="/"
-                >
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="/">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="/">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
+              {citiesList}
             </ul>
           </section>
         </div>
         <div className="cities">
-          {cardsCount > 0 ? (
-            <Cities cardsCount={cardsCount} cardsOnPage={cardsOnPage} />
-          ) : (
-            <CitiesEmpty />
-          )}
+          {offersList}
         </div>
       </main>
     </div>
