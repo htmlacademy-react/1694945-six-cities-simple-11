a5afc6a6c93c36d290ced9918363d012d44e9223
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-11 11:45:52 +0700

    Обновлены импорты, изменены обходы массивов

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -3,29 +3,25 @@ import HeaderSvg from '../../components/header/header-svg';
 import Header from '../../components/header/header';
 import OffersList from '../../components/offer/offers-list';
 import NoOffers from '../../components/offer/no-offers';
-import { Offers } from '../../types/offer';
+import { Offer } from '../../types/offer';
 import { CITIES, ACTIVE_CITY } from '../../const';
 
-
 type MainPageProps = {
-  offers: Offers;
+  offers: Offer[];
   isAuthorized: boolean;
 };
 
 function MainPage({ isAuthorized, offers }: MainPageProps): JSX.Element {
-  const filteredOffers = offers.filter((offer) => offer.city.name === ACTIVE_CITY);
+  const filteredOffers = offers.filter((offer) => offer.city.name === ACTIVE_CITY.name);
   const mainClassName =
     filteredOffers.length > 0
       ? 'page page--gray page--main'
       : 'page__main page__main--index page__main--index-empty';
-  const citiesList = CITIES.map((city, index) => ({
-    id: index + 1,
-    name: city,
-  })).map((city) => (
-    <li key={city.id} className="locations__item">
+  const citiesList = CITIES.map((city) => (
+    <li key={city.name} className="locations__item">
       <a
         className={`locations__item-link tabs__item
-          ${city.name === ACTIVE_CITY ? ' tabs__item--active' : ''}`}
+          ${city.name === ACTIVE_CITY.name ? ' tabs__item--active' : ''}`}
         href="#todo"
       >
         <span>{city.name}</span>
@@ -35,7 +31,7 @@ function MainPage({ isAuthorized, offers }: MainPageProps): JSX.Element {
   const offersList =
     offers.length > 0
       ? <OffersList offers={offers} city={ACTIVE_CITY} />
-      : <NoOffers city={ACTIVE_CITY} />;
+      : <NoOffers city={ACTIVE_CITY.name} />;
   return (
     <div className="page page--gray page--main">
       <Helmet>
