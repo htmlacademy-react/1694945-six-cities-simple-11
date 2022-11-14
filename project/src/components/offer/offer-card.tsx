32359086981d6d0440ffbc940af57ca0af922b02
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-14 14:47:19 +0700

    Обработчик перенесён из родительского компонента

--- project/src/components/offer/offer-card.tsx
+++ project/src/components/offer/offer-card.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import { Offer } from '../../types/offer';
 import CardPremiumMark from './card/card-premium-mark';
 import CardPreview from './card/card-preview';
@@ -8,28 +6,40 @@ import CardRating from './card/card-rating';
 import CardName from './card/card-name';
 import CardType from './card/card-type';
 
-
-type CardProps = {
+type CardComponentProps = {
   offer: Offer;
+  mouseOverHandler: () => void;
 };
 
-function OfferCard(props: CardProps): JSX.Element {
-  const { offer } = props;
-  const { previewImage, price, title, type, rating, isPremium, id } = offer;
-  const [activeCard, setActiveCard] = useState(0);
-  const mouseOverHandler = () => setActiveCard(id);
-  const link = `/offer/${activeCard}`;
+function OfferCard({ offer, mouseOverHandler }: CardComponentProps): JSX.Element {
+  const {
+    id,
+    previewImage,
+    price,
+    title,
+    type,
+    rating,
+    isPremium
+  } = offer;
+  const link = `/offer/${id}`;
   return (
     <article
       className="cities__card place-card"
       onMouseOver={mouseOverHandler}
     >
       {isPremium && <CardPremiumMark />}
-      <CardPreview link={link} previewImage={previewImage} title={title} />
+      <CardPreview
+        link={link}
+        previewImage={previewImage}
+        title={title}
+      />
       <div className="place-card__info">
         <CardPrice price={price} />
         <CardRating rating={rating} />
-        <CardName link={link} title={title} />
+        <CardName
+          link={link}
+          title={title}
+        />
         <CardType type={type} />
       </div>
     </article>
