b4e009691cf9a3baa793085621bfc9ad61c2d9e0
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-14 14:25:56 +0700

    Обновлены константы

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,4 +1,4 @@
-import {City} from './types/city';
+import { City } from './types/city';
 export enum AppRoute {
   Main = '/',
   Login = '/login',
@@ -10,12 +10,33 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
-export const accomodationType = {
+export enum Zoom {
+  ActiveCity = 10,
+  Location = 8,
+}
+export enum Icon {
+  DefaultURL = './img/pin.svg',
+  ActiveURL = './img/pin-active.svg',
+  Width = 28,
+  Height = 40,
+  AnchorWidth = 14,
+}
+export enum FormatDate {
+  Locale = 'en-US',
+  Month = 'long',
+  Year = 'numeric',
+}
+export enum ReviewData {
+  ListLength = 10,
+  RatingMax = 5,
+  RatingValue = 20,
+}
+export const ACCOMODAION_TYPES = Object.freeze({
   apartment: 'Apartment',
   room: 'Private room',
   house: 'House',
   hotel:'Hotel',
-};
+});
 export const MARKS = Object.freeze([
   {
     key: 5,
@@ -38,14 +59,14 @@ export const MARKS = Object.freeze([
     value: 'terribly',
   },
 ]);
-export const ZOOM_CITY = 10;
+
 export const CITIES: City[] = [
   {
     name: 'Paris',
     location: {
       lat: 48.8534100,
       lng: 2.3488000,
-      zoom: ZOOM_CITY,
+      zoom: Zoom.ActiveCity,
     }
   },
   {
@@ -53,7 +74,7 @@ export const CITIES: City[] = [
     location: {
       lat: 50.9333300,
       lng: 6.9500000,
-      zoom: ZOOM_CITY,
+      zoom: Zoom.ActiveCity,
     }
   },
   {
@@ -61,7 +82,7 @@ export const CITIES: City[] = [
     location: {
       lat: 50.8504500,
       lng: 4.3487800,
-      zoom: ZOOM_CITY,
+      zoom: Zoom.ActiveCity,
     }
   },
   {
@@ -69,7 +90,7 @@ export const CITIES: City[] = [
     location: {
       lat: 52.3740300,
       lng: 4.8896900,
-      zoom: ZOOM_CITY,
+      zoom: Zoom.ActiveCity,
     }
   },
   {
@@ -77,7 +98,7 @@ export const CITIES: City[] = [
     location: {
       lat: 53.5753200,
       lng: 10.0153400,
-      zoom: ZOOM_CITY,
+      zoom: Zoom.ActiveCity,
     }
   },
   {
@@ -85,7 +106,7 @@ export const CITIES: City[] = [
     location:{
       lat: 51.2217200,
       lng: 6.7761600,
-      zoom: ZOOM_CITY,
+      zoom: Zoom.ActiveCity,
     }
   }
 ];
@@ -97,9 +118,7 @@ export const SORT_TYPES = Object.freeze([
   'Top rated first',
 ]);
 export const ACTIVE_SORT = SORT_TYPES[0];
-export const LOCALE = 'en-US';
-export const FORMAT_MONTH = 'long';
-export const FORMAT_YEAR = 'numeric';
-export const REVIEWS_LIST_LENGTH = 10;
-export const RATING_MAX = 5;
-export const RATING_VALUE = 20;
+export const LAYER_OPTION = {
+  url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+};
