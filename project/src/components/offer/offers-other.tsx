842bfde1c7443aebf55e39d249e321ad02ef953b
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-14 14:48:35 +0700

    Добавлен обработчик, который будет вызываться в дочернем компоненте

--- project/src/components/offer/offers-other.tsx
+++ project/src/components/offer/offers-other.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import { Offer } from '../../types/offer';
 import OfferCard from './offer-card';
 type OffersOtherComponentProps = {
   offers: Offer[];
 };
 function OffersOther({ offers }: OffersOtherComponentProps): JSX.Element {
-  const offerCardsList = offers.map(
-    (offer) => <OfferCard key={offer.id} offer={offer} />
-  );
+  const [, setActiveCard] = useState(0);
+  const cards = offers.map((offer) => (
+    <OfferCard
+      key={offer.id}
+      offer={offer}
+      mouseOverHandler={() => setActiveCard(offer.id)}
+    />
+  ));
   return (
     <section className="near-places places">
       <h2 className="near-places__title">
         Other places in the neighbourhood
       </h2>
       <div className="near-places__list places__list">
-        {offerCardsList}
+        {cards}
       </div>
     </section>
   );
