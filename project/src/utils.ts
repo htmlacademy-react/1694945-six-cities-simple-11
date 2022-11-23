34d9160dea461631d428d336533b3553914bc4d1
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-23 09:53:58 +0700

    Переделана функция получения сортированного массива отзывов

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -1,20 +1,10 @@
-import {
-  FormatDate,
-  Rating,
-  REVIEWS_LIST_LENGTH
-} from './const';
+import { FormatDate, Rating, REVIEWS_LIST_LENGTH } from './const';
 import { Review } from './types/review';
 
-export const getPluralWord = (
-  number: number,
-  word: string
-): string =>
+export const getPluralWord = (number: number, word: string): string =>
   `${word}${number > 1 ? 's' : ''}`;
 
-export const formatDate = (
-  date: string,
-  locales = FormatDate.Locale
-): string =>
+export const formatDate = (date: string, locales = FormatDate.Locale): string =>
   new Date(date).toLocaleString(locales, {
     month: FormatDate.Month,
     year: FormatDate.Year,
@@ -27,7 +17,5 @@ export const getSortedReviews = (reviews: Review[]): Review[] => {
   const sortedReviews = reviews.sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   );
-  return reviews.length > REVIEWS_LIST_LENGTH
-    ? sortedReviews.slice(REVIEWS_LIST_LENGTH)
-    : sortedReviews;
+  return sortedReviews.slice(0, REVIEWS_LIST_LENGTH);
 };
