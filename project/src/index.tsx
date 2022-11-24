7ef90b7cf4a542aba5a92ff265fad0d0111c82b0
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-24 19:45:29 +0700

    Подключен 'redux'

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
 import { reviews } from './mocks/reviews';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App
-      offers={offers}
-      reviews={reviews}
-    />
+    <Provider store={store}>
+      <App
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>
   </React.StrictMode>,
 );
