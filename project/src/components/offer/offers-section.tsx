e2687761761227c3e2ab8b83b1516604ab8a3306
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-29 10:08:55 +0700

    Активный город теперь — входящее свойство

--- project/src/components/offer/offers-section.tsx
+++ project/src/components/offer/offers-section.tsx
@@ -1,14 +1,14 @@
+import { City } from '../../types/city';
 import { Offer } from '../../types/offer';
-import { useAppSelector } from '../../hooks/use-app-selector';
 import OffersList from '../../components/offer/offers-list';
 import NoOffers from '../../components/offer/no-offers';
 
 type OffersSectionProps = {
+  activeCity: City;
   offers: Offer[];
 };
 
-function OffersSection({ offers }: OffersSectionProps): JSX.Element {
-  const activeCity = useAppSelector((state) => state.activeCity);
+function OffersSection({ activeCity, offers }: OffersSectionProps): JSX.Element {
   return offers.length > 0
     ? <OffersList offers={offers} city={activeCity} />
     : <NoOffers city={activeCity.name} />;
