fc367dc416cd7fa2e1b0dc90b5de4c1681bfbe1c
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-11-30 20:32:50 +0700

    Небольшая правка

--- project/src/store/selectors.ts
+++ project/src/store/selectors.ts
@@ -1,10 +1,9 @@
 import { State } from '../types/state';
 import { orderOffersByType, getOffersByCity } from '../utils';
-const getSortedOffers = (state: State) => orderOffersByType(
+export const getSortedOffers = (state: State) => orderOffersByType(
   getOffersByCity(
     state.offers,
     state.activeCity.name
   ),
   state.activeSort
 );
-export default getSortedOffers;
