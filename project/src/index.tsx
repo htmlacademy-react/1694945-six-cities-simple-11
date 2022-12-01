2e1c39a71a5414f4d28d54eb2d73498863775dad
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-12-01 13:31:29 +0700

    Добавлена обработка ошибок взаимодействия с сервером

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
-import ErrorMessage from './components/error-message/error-message';
 import App from './components/app/app';
+import ErrorMessage from './components/error-message/error-message';
+import { store } from './store/store';
+import { checkAuthAction } from './store/api-actions';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+store.dispatch(checkAuthAction());
 
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,22 +1,47 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
-import { BACKEND_URL, REQUEST_TIMEOUT } from '../const';
+import axios, {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosError,
+} from 'axios';
+import {
+  BACKEND_URL,
+  REQUEST_TIMEOUT
+} from '../const';
+import { StatusCodes } from 'http-status-codes';
 import { getToken } from './token';
+import { processErrorHandle } from './process-error-handle';
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true,
+};
+
+const shouldDisplayError = (response: AxiosResponse) =>
+  !!StatusCodeMapping[response.status];
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
-    timeout: REQUEST_TIMEOUT
+    timeout: REQUEST_TIMEOUT,
+  });
+  api.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+    return config;
   });
 
-  api.interceptors.request.use(
-    (config: AxiosRequestConfig) => {
-      const token = getToken();
-
-      if (token && config.headers) {
-        config.headers['x-token'] = token;
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{ error: string }>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        processErrorHandle(error.response.data.error);
       }
 
-      return config;
+      throw error;
     }
   );
 
