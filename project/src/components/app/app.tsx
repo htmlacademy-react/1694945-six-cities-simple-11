dab9fcfd5cee63fb99dd78934e5fbe4b8413049a
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-12-01 18:03:46 +0700

    Перемещены вызовы 'dispatch'

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,24 +1,14 @@
-import { useEffect } from 'react';
 import HistoryRouter from '../history-router/history-router';
 import { Routes, Route } from 'react-router-dom';
 import { browserHistory } from '../../browser-history';
 import { HelmetProvider } from 'react-helmet-async';
 import { AppRoute } from '../../const';
-import { checkAuthorizeAction, fetchOffersAction } from '../../store/api-actions';
-import { useAppDispatch } from '../../hooks/use-app-dispatch';
 import MainPage from '../../pages/main-page/main-page';
 import LoginPage from '../../pages/login-page/login-page';
 import PropertyPage from '../../pages/property-page/property-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 function App(): JSX.Element {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(checkAuthorizeAction());
-    dispatch(fetchOffersAction());
-  }, [dispatch]);
-
   return (
     <HelmetProvider>
       <HistoryRouter history={browserHistory}>
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { checkAuthorizeAction, fetchOffersAction } from './store/api-actions';
 import App from './components/app/app';
 import ErrorMessage from './components/error-message/error-message';
-import { store } from './store/store';
-import { checkAuthorizeAction } from './store/api-actions';
 
 store.dispatch(checkAuthorizeAction());
+store.dispatch(fetchOffersAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
