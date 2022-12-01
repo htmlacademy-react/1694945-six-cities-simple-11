c8709a0062cd58550601543e4f318a359725883b
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-12-01 13:04:28 +0700

    Создан и подключен компонент для отображения ошибки

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import ErrorMessage from './components/error-message/error-message';
 import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ErrorMessage />
       <App />
     </Provider>
   </React.StrictMode>,
