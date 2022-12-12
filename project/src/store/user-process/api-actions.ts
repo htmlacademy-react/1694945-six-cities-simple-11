c054493f9ad552db6d93c5ef639c51873b25b6ae
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-12-12 16:23:49 +0700

    Созданы тесты для асинхронных действий в 'userProcess'

--- project/src/store/user-process/api-actions.ts
+++ project/src/store/user-process/api-actions.ts
@@ -6,39 +6,45 @@ import { UserData } from '../../types/user-data';
 import { APIRoute } from '../../const';
 import { saveToken, dropToken } from '../../services/token';
 
-export const checkAuthorizeAction = createAsyncThunk<UserData, undefined, {
-  dispatch: AppDispatch;
-  state: State;
-  extra: AxiosInstance;
-}>(
-  'user/checkAuthorization',
-  async (_arg, {extra: api}) => {
-    const {data} = await api.get<UserData>(APIRoute.Login);
-    return data;
+export const checkAuthorizeAction = createAsyncThunk<
+  UserData,
+  undefined,
+  {
+    dispatch: AppDispatch;
+    state: State;
+    extra: AxiosInstance;
   }
-);
+>('user/checkAuthorization', async (_arg, { extra: api }) => {
+  const { data } = await api.get<UserData>(APIRoute.Login);
+  return data;
+});
 
-export const loginAction = createAsyncThunk<UserData, AuthData, {
-  dispatch: AppDispatch;
-  state: State;
-  extra: AxiosInstance;
-}>(
-  'user/login',
-  async ({login: email, password}, {extra: api}) => {
-    const {data} = await api.post<UserData>(APIRoute.Login, {email, password});
-    saveToken(data.token);
-    return data;
+export const loginAction = createAsyncThunk<
+  UserData,
+  AuthData,
+  {
+    dispatch: AppDispatch;
+    state: State;
+    extra: AxiosInstance;
   }
-);
+>('user/login', async ({ login: email, password }, { extra: api }) => {
+  const { data } = await api.post<UserData>(APIRoute.Login, {
+    email,
+    password,
+  });
+  saveToken(data.token);
+  return data;
+});
 
-export const logoutAction = createAsyncThunk<void, undefined, {
-  dispatch: AppDispatch;
-  state: State;
-  extra: AxiosInstance;
-}>(
-  'user/logout',
-  async (_arg, {extra: api}) => {
-    await api.delete(APIRoute.Logout);
-    dropToken();
+export const logoutAction = createAsyncThunk<
+  void,
+  undefined,
+  {
+    dispatch: AppDispatch;
+    state: State;
+    extra: AxiosInstance;
   }
-);
+>('user/logout', async (_arg, { extra: api }) => {
+  await api.delete(APIRoute.Logout);
+  dropToken();
+});
