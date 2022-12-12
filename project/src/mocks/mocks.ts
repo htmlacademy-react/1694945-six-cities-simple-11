5becf6f5226fca193ab5ad4f30ced0907e0919b6
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-12-12 09:55:55 +0700

    Добавлены тестовые данные

---
+++ project/src/mocks/mocks.ts
@@ -0,0 +1,77 @@
+import {
+  datatype,
+  commerce,
+  image,
+  internet,
+  address,
+  lorem,
+  date,
+} from 'faker';
+import { User } from '../types/user';
+import { UserData } from '../types/user-data';
+import { Location } from '../types/location';
+import { City } from '../types/city';
+import { Offer } from '../types/offer';
+import { Review } from '../types/review';
+
+export const makeFakeUser = (): User => ({
+  id: datatype.number(),
+  name: internet.userName(),
+  isPro: datatype.boolean(),
+  avatarUrl: internet.avatar(),
+});
+
+export const makeFakeUserData = (): UserData => ({
+  id: datatype.number(),
+  name: internet.userName(),
+  isPro: datatype.boolean(),
+  avatarUrl: internet.avatar(),
+  email: internet.email(),
+  token: datatype.string(),
+});
+
+export const makeFakeLocation = (): Location => ({
+  zoom: datatype.number({ min: 5, max: 15 }),
+  latitude: datatype.number({ min: 1, max: 10, precision: 0.0001 }),
+  longitude: datatype.number({ min: 1, max: 10, precision: 0.0001 }),
+});
+
+export const makeFakeCity = (): City => ({
+  name: address.city(),
+  location: makeFakeLocation(),
+});
+
+export const makeFakeOffer = (): Offer => ({
+  id: datatype.number(),
+  isPremium: datatype.boolean(),
+  title: commerce.productName(),
+  type: commerce.product(),
+  price: datatype.number(),
+  rating: datatype.number({ min: 1, max: 5, precision: 0.1 }),
+  previewImage: image.imageUrl(),
+  images: Array.from({ length: 6 }, () => image.imageUrl()),
+  bedrooms: datatype.number({ min: 1, max: 5 }),
+  maxAdults: datatype.number({ min: 1, max: 5 }),
+  goods: Array.from({ length: 10 }, () => commerce.product()),
+  description: commerce.productDescription(),
+  host: makeFakeUser(),
+  city: makeFakeCity(),
+  location: makeFakeLocation(),
+});
+
+export const makeFakeOffers = (): Offer[] =>
+  Array.from({ length: 10 }, makeFakeOffer);
+
+export const makeFakeOtherOffers = (): Offer[] =>
+  Array.from({ length: 3 }, makeFakeOffer);
+
+export const makeFakeReview = (): Review => ({
+  id: datatype.number(),
+  user: makeFakeUser(),
+  rating: datatype.number({ min: 1, max: 5, precision: 0.1 }),
+  comment: lorem.sentence(),
+  date: String(date.recent()),
+});
+
+export const makeFakeReviews = (): Review[] =>
+  Array.from({ length: 10 }, makeFakeReview);
