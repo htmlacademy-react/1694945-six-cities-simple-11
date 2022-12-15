dfb60873ca91d3f4ca595503663d4fffc25da63f
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2022-12-15 15:34:12 +0700

    Переименован корректно тестовый файл

--- project/src/components/cities/cities.test.tsx
+++ project/src/components/cities/cities.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
-import HistoryRouter from '../../components/history-router/history-router';
+import HistoryRouter from '../history-router/history-router';
 import { createMemoryHistory } from 'history';
 import { CITIES, ACTIVE_CITY, ACTIVE_SORT } from '../../const';
 import Cities from './cities';
